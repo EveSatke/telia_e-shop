9cf75c52ee2a2b3dbdd448b61c323cb7f3529718
Evelina Satkauske <evelina.satkauske@gmail.com>
2025-06-05 15:03:17 +0300

    Updates

---
+++ _next/static/chunks/app/layout-6f6e84d5fd32b7a6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5409:function(e,n,t){Promise.resolve().then(t.t.bind(t,8173,23)),Promise.resolve().then(t.t.bind(t,231,23)),Promise.resolve().then(t.t.bind(t,3069,23)),Promise.resolve().then(t.t.bind(t,8877,23))},8877:function(){}},function(e){e.O(0,[783,173,614,971,23,744],function(){return e(e.s=5409)}),_N_E=e.O()}]);
